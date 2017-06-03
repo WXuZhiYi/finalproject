d199ca2859d169ee6ed52c374cda572abdcada50
xuzhiyiyi <1441218265@qq.com>
2017-06-03 18:08:06 +0800

    第二次提交

--- src/user/main.c
+++ src/user/main.c
@@ -1,15 +1,54 @@
 #include "LPC11xx.h"
 #include "oled.h"
+#include "ui.h"
 #include "adc.h"
+#include "key.h"
+#include "BEEP.H"
 #include "uart.h"
 #include "delay.h"
+#include "i2c.h"
+#include "24c02.h"
+#include "temperature.h"
+
+extern  float low_temp ;
+extern 	float high_temp ;
+extern 	int   lastlow_temp ;
+extern 	int   lasthigh_temp ; 
+	
+
+
+
+
+
+extern int key_value;
 int main()
 {
+	
+	ADC_Init(7);
+	beep_init();
+	UART_Init();										
+	I2C_Init(1);			
 	OLED_Init();
+	key_init();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	low_temp= AT24C02_ReadOneByte(0x01);
+	high_temp= AT24C02_ReadOneByte(0x02);
 	while(1)
 	{
-	
-	
+	   
+		UI();
 	}
 	
 
